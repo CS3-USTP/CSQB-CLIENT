f92f2c3825677b576bf2cb55d2528a688a14b1d9
mihkuno <caindayjoeninyo@gmail.com>
2024-08-13 00:18:29 +0800

    capitalize Client

--- src/CubeExecutable/Program.cs
+++ src/CubeExecutable/Program.cs
@@ -8,7 +8,7 @@ namespace CubeExecutable
 
         // TODO: On deployment, change path of executables
         static readonly string wtExecPath = "C:/Users/LENOVO/Desktop/CSQB-CLIENT/bin/wt.exe";   
-        static readonly string cubeExecPath = "C:/Users/LENOVO/Desktop/CSQB-CLIENT/src/CubeClient/bin/Debug/net8.0/client.exe";
+        static readonly string cubeExecPath = "C:/Users/LENOVO/Desktop/CSQB-CLIENT/src/CubeClient/bin/Debug/net8.0/Client.exe";
 
         static void Main(string[] args)
         {            
